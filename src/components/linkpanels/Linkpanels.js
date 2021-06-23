b323e1749438e7a423788f543a186e29a039418e
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-06-23 09:58:50 +0200

    Fiks lenker til undersider av appen

--- src/components/linkpanels/Linkpanels.js
+++ src/components/linkpanels/Linkpanels.js
@@ -4,10 +4,16 @@ import styles from "./Linkpanels.module.css";
 export default function Linkpanels() {
   return (
     <section id={styles.linkpanels}>
-      <LinkPanel className={styles.linkpanel} href="/brukernotifikasjoner">
+      <LinkPanel
+        className={styles.linkpanel}
+        href="/person/tms-prototyper/brukernotifikasjoner"
+      >
         Brukernotifikasjoner
       </LinkPanel>
-      <LinkPanel className={styles.linkpanel} href="/dokumenter">
+      <LinkPanel
+        className={styles.linkpanel}
+        href="/person/tms-prototyper/dokumenter"
+      >
         Dokumenter
       </LinkPanel>
     </section>
