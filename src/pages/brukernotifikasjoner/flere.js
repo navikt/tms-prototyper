f0ceced4d776054e42f4ae8b22c26d6c733cdedd
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-06-28 17:08:07 +0200

    Fikser build feil

---
+++ src/pages/brukernotifikasjoner/flere.js
@@ -0,0 +1,21 @@
+import {
+  Alert,
+  Panel,
+  LinkPanel,
+  Link,
+  Title,
+  Ingress,
+} from "@navikt/ds-react";
+import styles from "../../components/linkpanels/Linkpanels.module.css";
+
+export default function Flere() {
+  return (
+    <section>
+      <Panel>
+        <Alert variant="info">Beskjed</Alert>
+        <Alert variant="warning">Oppgave</Alert>
+        <Alert variant="success">Statusoppdatering</Alert>
+      </Panel>
+    </section>
+  );
+}
